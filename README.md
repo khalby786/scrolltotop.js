ce6719f28c398182d6d66b10a23d5e6ada84bd5e
Glitch (back-to-top-demo) <none>
2019-07-28 08:37:38 +0000

    New options!

--- README.md
+++ README.md
@@ -1,12 +1,19 @@
 # scrolltotop.js
 
-<b>scrolltotop.js</b> is a lightweight interactive scroll to top button which requires the calling of just a function: ```backtotop()```.
+<b>scrolltotop.js</b> is a lightweight interactive scroll to top button which requires the calling of just a function: ```scrolltotop()```.
 
-```backtotop()``` has four parameters:
+```scrolltotop()``` has four parameters:
 * ```text```
 * ```backimage```
 * ```backcolor```
-* ```cornerround```
+* ```cornerround``` (the border radius)
 
-Include ```backtotop(text, image, backcolor, cornerround)``` by either download the full version here or by including 
-```<script src=""></script>```
+```scrolltotop(text, image, backcolor, cornerround)```
+
+Include the library by either downloading the full version [here](https://raw.githubusercontent.com/khalby786/scrolltotop.js/master/script.js) or by including 
+
+```<script src= 'https://raw.githubusercontent.com/khalby786/scrolltotop.js/master/script.js'></script>```
+
+and call the function ```scrolltotop('Hello', 'image.jpg', 'red', '50%')``` (for example).
+
+Typing the word _default_ enclosed in apostrophes for the first three parameters can invoke default values and design for the ScrollToTop button.
--- script.js
+++ script.js
@@ -1,55 +1,68 @@
   /* If you're feeling fancy you can add interactivity 
     to your site with Javascript */
 
-      // prints "hi" in the browser's dev tools console
-      function backtotop (text, backimage, backcolor, cornerround) {
-        var scrolldiv = document.createElement('div');
-        scrolldiv.setAttribute("id", "totop")
-        document.body.appendChild(scrolldiv);
-        var scrollDiv = document.getElementById("totop");
-        scrollDiv.style.position = "fixed";
-        scrollDiv.style.bottom = "20px";
-        scrollDiv.style.right = "30px";
-        scrollDiv.style.zIndex = "99";
-        scrollDiv.style.backgroundColor = backcolor;
-        scrollDiv.style.cursor = "pointer";
-        scrollDiv.style.padding = "15px";
-        scrollDiv.style.borderRadius = cornerround;
-        scrollDiv.textContent = text;
-        scrollDiv.style.backgroundImage = backimage;
-        scrollDiv.style.backgroundRepeat = "no-repeat";
-        scrollDiv.style.backgroundPosition = "50% 50%";
-        scrollDiv.backgroundColor = "transparent";
-        scrollDiv.style.backgroundSize = "100%";
-        scrollDiv.style.height = "20px";
-        scrollDiv.style.width = "20px";
-        scrollDiv.setAttribute("onclick", "topFunction()");
-      }
-      
-      function backtotopHeightWwidth(h, w) {
-        var scrollDiv = document.getElementById("totop");
-        scrollDiv.style.height = h;
-        scrollDiv.style.width = w;
-      }
+// prints "hi" in the browser's dev tools console
+function scrolltotop (text, backimage, backcolor, cornerround) {
+  var scrolldiv = document.createElement('div');
+  scrolldiv.setAttribute("id", "totop")
+  document.body.appendChild(scrolldiv);
+  var scrollDiv = document.getElementById("totop");
+  scrollDiv.style.position = "fixed";
+  scrollDiv.style.bottom = "20px";
+  scrollDiv.style.right = "30px";
+  scrollDiv.style.zIndex = "99";
+  if (backimage === "default") {
+    backimage = "none";
+  }
+  
+  if (backcolor === "default") {
+    backcolor = "white";
+  }
+  
+  if (text === "default") {
+    text = "↑";
+  }
+        
+  scrollDiv.style.backgroundColor = backcolor;
+  scrollDiv.style.cursor = "pointer";
+  scrollDiv.style.padding = "15px";
+  scrollDiv.style.borderRadius = cornerround;
+  scrollDiv.style.textAlign = "center";
+  scrollDiv.textContent = text;
+  scrollDiv.style.backgroundImage = backimage;
+  scrollDiv.style.backgroundRepeat = "no-repeat";
+  scrollDiv.style.backgroundPosition = "50% 50%";
+  scrollDiv.backgroundColor = "transparent";
+  scrollDiv.style.backgroundSize = "100%";
+  scrollDiv.style.height = "20px";
+  scrollDiv.style.width = "20px";
+  scrollDiv.setAttribute("onclick", "topFunction()");
+}
 
-      window.onscroll = (function(){ 
-        scrollFunction() 
-      });
+function backtotopHeightWwidth(h, w) {
+  var scrollDiv = document.getElementById("totop");
+  scrollDiv.style.height = h;
+  scrollDiv.style.width = w;
+}
 
-      function scrollFunction() {
-        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-          var totopEl = document.getElementById("totop");
-          totopEl.style.display = "block";
-        } else {
-          var totopEll = document.getElementById("totop");
-          totopEll.style.display = "none";
-        }
-      }
+window.onscroll = (function(){ 
+   scrollFunction() 
+});
 
-      // When the user clicks on the button, scroll to the top of the document
-      function topFunction() {
-        document.body.scrollTop = 0; // For Safari
-        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-      }
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    var totopEl = document.getElementById("totop");
+    totopEl.style.display = "block";
+  } else {
+    var totopEll = document.getElementById("totop");
+    totopEll.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
 
       //h, back, cornerround
